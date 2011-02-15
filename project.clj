f8a6256e015405d45066bd223428b579d8a7e7af
Jozef Wagner <jozef.wagner@gmail.com>
2011-02-15 14:33:03 +0100

    use incanter for exporting to excel

--- project.clj
+++ project.clj
@@ -7,5 +7,6 @@
                  [compojure "0.6.0-RC4"]
                  [ring/ring-jetty-adapter "0.3.5"]
                  [hiccup "0.3.4"]
-                 [clj-time "0.3.0-SNAPSHOT"]]
+                 [clj-time "0.3.0-SNAPSHOT"]
+                 [incanter "1.2.3"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
