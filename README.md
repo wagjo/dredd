9f4580abfb30f46d7fabb46614c2ec6ff56a13eb
Jozef Wagner <jozef.wagner@gmail.com>
2011-02-24 16:39:56 +0100

    docs

--- README.md
+++ README.md
@@ -1,23 +1,21 @@
 # dredd
 
-FIXME: write description
+Simple Online Automated Judge System in Clojure. Mainly used in my Programming course so many things in it are ad-hoc. Moreover, many texts are not in english language, as it is not used in english speaking country.
 
 ## Usage
 
-starting:
+* Copy dredd.local_settings_example.clj into dredd.local_settings.clj and customize it
 
-screen ./start.sh
+* Starting dredd:
+    * screen ./start.sh
+    * then press C-a d
 
-then press C-a d
-
-shutdown:
-
-resume with screen -r
-
-then C-c to shutdown server
+* Shutdown:
+    * resume with screen -r
+    * then C-c to shutdown server
 
 ## License
 
-Copyright (C) 2010 FIXME
+Copyright (C) 2011, Jozef Wagner. All rights reserved.
 
-Distributed under the Eclipse Public License, the same as Clojure.
+Contact: <jozef.wagner@gmail.com> or <wagjo@wagjo.com>
--- src/dredd/core.clj
+++ src/dredd/core.clj
@@ -1,39 +1,67 @@
+;; Copyright (C) 2011, Jozef Wagner. All rights reserved. 
+
 (ns dredd.core
-  "Main entry point to the app"
+  "Main entry point to the dredd"
   (:require [dredd.app :as app]
             [dredd.data.core :as data]
             [dredd.db-adapter.neo4j :as neo]
             [ring.adapter.jetty :as adapter]))
 
-;; Server management
+;;;; Overview
+
+;; Main entry point in the dredd. Its purpose is to:
+;; - Establish connection to database
+;; - Initialize database if needed
+;; - Start and manage web server which runs the dredd
+;;
+;;; Usage notes
+;; - You use this ns to start and stop dredd.
+;; - See README file for usage examples.
+;; - Servlets and Application containers are not supported.
+;; - Start dredd on a dedicated port and use reverse proxy to
+;;   make it public. That way you can also enable TLS for dredd.
+;;
+;;; Code layout and notes
+;; - Application layout is handled in dredd.app
+;; - Data initialization is handled in dredd.data.core
+;; - Jetty is used as a web server
+;; - Neo4j is used as a database
+
+
+;;;; Implementation details
 
 (defonce *server* nil)
 
+;;;; Public API
+
+;;; Server management
+
 (defn start []
-  "start the app"
+  "Start the app"
   (neo/start-neo)
   (data/init!)
   (let [server (adapter/run-jetty app/app {:port 3000 :join? false})]
     (alter-var-root #'*server* (fn [_] server))))
 
 (defn stop []
-  "stop the running app"
+  "Stop the running app"
   (.stop *server*)
   (neo/stop-neo))
 
 (defn start-and-wait []
-  "start the app and block"
-  (neo/start-neo)
-  (data/init!)
-  (adapter/run-jetty app/app {:port 3000})
-  (neo/stop-neo))
+  "Start the app and block"
+  (start)
+  (.join *server*)
+  (stop))
 
-;; Examples
+;;;; Examples
 
 (comment
 
   (start)
 
   (stop)
+
+  (start-and-wait)
   
 )
